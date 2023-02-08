dbd8bd0c1b504dd1e1d1c28197b90df688355294
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2023-02-08 20:08:00 -0300

    [class] - reviewing python basics
    it's been a while since I last practiced.

---
+++ Day_1/a_printing.py
@@ -0,0 +1,10 @@
+# Write your code below this line 👇
+print("Hello, World!")
+print()
+
+print("Hello, World!\nHello, World!\nHello, World!")
+print()
+
+print("""Hello, World
+Hello, World!
+Hello, World!""")
