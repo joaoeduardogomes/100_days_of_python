be95bd72a1c940385b8dde3b10f780e338f41234
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2023-02-14 13:55:36 -0300

    [Day 1 project] - added

---
+++ Day_1/project.py
@@ -0,0 +1,13 @@
+"""
+Este projeto consiste em criar um nome de banda baseado na cidade natal do usuário e no nome do seu pet.
+"""
+
+#1. Create a greeting for your program.
+print("Welcome to the band name generator program")
+#2. Ask the user for the city that they grew up in.
+city_name = input("\nLet's start with your hometown name: \n")
+#3. Ask the user for the name of a pet.
+pet_name = input("\nNow, tell me what is your pet name: \n")
+#4. Combine the name of their city and pet and show them their band name.
+print(f"\nI just came out with your band name. What about '{city_name} {pet_name}'?")
+#5. Make sure the input cursor shows on a new line:
