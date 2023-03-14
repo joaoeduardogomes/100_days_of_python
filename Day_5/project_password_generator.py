f65d37c0a013d11b968e3abf8cedaabc8720c394
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2023-03-13 21:59:21 -0300

    [Day 5] project done

---
+++ Day_5/project_password_generator.py
@@ -0,0 +1,42 @@
+"""
+O projeto consiste em criar uma senha com a quantidade de letras, números e símbolos que o usuário desejar.
+"""
+
+from random import choice, shuffle
+
+#Password Generator Project
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+password = ""
+
+for letter in range (1, nr_letters + 1):
+    pick = choice(letters)
+    password += pick
+
+for number in range (1, nr_numbers + 1):
+    pick = choice(numbers)
+    password += pick
+
+for symbol in range (1, nr_symbols + 1):
+    pick = choice(symbols)
+    password += pick
+
+chars_list = list(password) #precisei criar uma lista porque não se pode usar 'shuffle' em string.
+shuffle(chars_list)
+randomized_password = "".join(chars_list)
+
+print(randomized_password)
